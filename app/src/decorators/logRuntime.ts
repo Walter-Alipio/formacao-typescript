9ddff3b34a44190a3848af2ca01f2f2e51d7c88b
Walter <silvawalteralipio@gmail.com>
2022-08-27 18:07:10 -0300

    add comments

--- app/src/decorators/logRuntime.ts
+++ app/src/decorators/logRuntime.ts
@@ -1,15 +1,21 @@
-export function LogRuntime(){
+export function LogRuntime(inSeconds: boolean = false){
   return function(
-    target: any,
-    propertykey: string,
-    descriptor: PropertyDescriptor
+    target: any,  //É a função construtora (método estático) ou o Prototype
+    propertyKey: string,  //Nome do método que está sendo decorado
+    descriptor: PropertyDescriptor  //Tem a referência para o método original
   ) {
     const origianlMethod = descriptor.value;
     descriptor.value = function(...args: any[]){
+      let divider = 1;
+      let unit = 'milissegundos';
+      if(inSeconds){
+        divider = 1000;
+        unit = 'segundos  '
+      }
       const temp1 = performance.now();
       const origianlReturn = origianlMethod.apply(this, args);//chama método original
       const temp2 = performance.now();
-      console.log(`${propertykey}, tempo de execução: ${(temp2-temp1)/1000} segundos`);
+      console.log(`${propertyKey}, tempo de execução: ${(temp2-temp1)/divider} ${unit}`);
       origianlReturn
     }
     return descriptor;
